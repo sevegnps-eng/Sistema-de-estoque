b2cc7c5c52b3b58149a8a764476d727585dce4c8
sevegnps-eng <sevegnps@gmail.com>
2026-02-22 21:57:15 -0300

    Atualizações de armazenamento e atualização de estoque

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ estoque = []
 
 def armazenar():
     
-    produto = input("Qual produto deseja armazenar? ")
+    produto = input("Qual produto deseja armazenar? ").lower()
     quantidade = int(input("Qual quantidade deseja armazenar? "))
     valor = (float(input("Qual valor deseja colocar no produto? ")))  
 
     for itens in estoque:
-        if ["Produto"] == produto:
+        if itens["Produto"] == produto:
             print("Esse produto já existe no estoque, atualizando quantidade. ")
             itens["Quantidade"] += quantidade
-
+            
             return estoque
 
     estoque.append({
@@ -24,13 +24,26 @@ def armazenar():
 
     return estoque
 
+# Listar Produtos
+
 def listar_produtos():
     
     for itens in estoque:
         print(itens)
 
-    
+# Remover Produtos
 
+def remover_produtos():
+   
+   for itens in estoque:
+      
+      item = input("Qual produto deseja remover? ")
+      
+      if item in itens == item:
+         del itens["Produto"]
+
+
+      return
         
             
 # Programa principal
@@ -39,25 +52,26 @@ def programa_principal():
 
    while True:
     
-      opcao = int(input("\n[1] Adicionar Produto \n[2] Remover produto \n[3] Listar produtos \n[4] Sair do programa \nSelecione uma opcão: "))
+      opcao = int(input("\n[1] Adicionar Produto \n[2] Remover produto \n[3] Listar produtos \n[4] Sair do programa \nSelecione uma opcão: ")) 
 
       if opcao == 1:
        armazenar()
 
       if opcao == 2:
-       pass
+       remover_produtos()
    
       if opcao == 3:
        listar_produtos()
 
       if opcao == 4:
+         print("Saindo do estoque...")
          break
-  
-       
-   
-
-        
-
+      
+      else:
+         
+         if opcao > 4:
+            print("Opção inválida")
+      
 programa_principal()
 
 
